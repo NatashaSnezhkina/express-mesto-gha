30f966e3afb5c7b864a4bd0d72cf88ecb0248357
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-04-27 18:36:35 +0300

    13pr after 1st rewiev

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '6268269dc4c2d10494d64451',
