5f9cd4229d27188eb3fe1d3216167f885c4c1be0
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-05-13 12:28:37 +0300

    before 1st review 3

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const {
   getUser,
   getUsers,
@@ -9,8 +11,33 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getUser);
-router.get('/:userId', getUserById);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required()
+      .custom((value, helpers) => {
+        if (validator.isURL(value)) {
+          return value;
+        }
+        return helpers.message('Заполните поле корректным URL');
+      })
+      .message({
+        'string.required': 'Поле должны быть заполнено',
+      }),
+  }),
+}), updateAvatar);
 
 module.exports = router;
