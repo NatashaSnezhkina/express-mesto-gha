e693837bcefb6f2d98962d6d14f7ea095f7961a4
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-05-17 19:22:12 +0300

    15pr

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,6 +10,7 @@ const {
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
+const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/404-error');
 
@@ -19,38 +19,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'http://api.mesto.natasha.snezh.nomoredomains.xyz',
-  'http://api.mesto.natasha.snezh.nomoredomains.xyz/signin',
-  'http://api.mesto.natasha.snezh.nomoredomains.xyz/signup',
-  'https://api.mesto.natasha.snezh.nomoredomains.xyz',
-  'https://api.mesto.natasha.snezh.nomoredomains.xyz/signin',
-  'https://api.mesto.natasha.snezh.nomoredomains.xyz/signup',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    return res.end();
-  }
-
-  return next();
-});
 app.use(cors());
 app.use(requestLogger);
 app.use(bodyParser.json());
