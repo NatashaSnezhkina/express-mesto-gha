f4fb368a885b7b4acad6295220977a4ee60f0054
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-05-17 19:14:31 +0300

    15pr

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -50,7 +51,7 @@ app.use((req, res, next) => {
 
   return next();
 });
-
+app.use(cors());
 app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
