ca80000443a28972b18a705604bbb6ca3bed1aa6
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-05-18 23:08:09 +0300

    15pr

--- controllers/users.js
+++ controllers/users.js
@@ -155,7 +155,7 @@ module.exports.login = (req, res, next) => {
       res.send({ token });
     })
     .catch(() => {
-      next(new AuthError(`${email}'Неверно указана электронная почта или пороль'`));
+      next(new AuthError(`${password}'Неверно указана электронная почта или пороль'`));
     });
 };
 
