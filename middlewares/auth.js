74211f78278b1617d126bc661fb35dbf9d599171
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2022-05-18 12:24:18 +0300

    15pr

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,24 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/401-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// const JWT_SECRET = 'secret-key';
-
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-  try {
-    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
-  } catch (err) {
-    throw new AuthError('Вы не авторизованы');
+  const { authorization } = req.headers;
+  // убеждаемся, что он есть или начинается с Bearer
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Необходима авторизация');
   }
 
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    throw new AuthError('Необходима авторизация');
+  }
   req.user = payload;
   next();
 };
